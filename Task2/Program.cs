d1cb2141042870d4d72745fcd3246fc1120c1cde
cham <nchaminn@mail.ru>
2022-05-02 19:07:41 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число А: ");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if  (numberA * numberA == numberB) 
+
+ {
+   Console.WriteLine("да");
+ } 
+
+else 
+Console.WriteLine("нет");
+
