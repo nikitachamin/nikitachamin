0e8f8251bad8c1932a29af809af47fd470cfd832
cham <nchaminn@mail.ru>
2022-05-13 12:32:19 +0300

    task1And2

---
+++ hometask1/Program.cs
@@ -0,0 +1,8 @@
+﻿int rundomNumber = new Random().Next(100,1000);
+
+Console.WriteLine($"Изначальное число {rundomNumber}");
+
+int numberOne = rundomNumber / 10 % 10;
+
+
+Console.WriteLine($"{numberOne}");
---
+++ hometask2/Program.cs
@@ -0,0 +1,25 @@
+﻿int rundomNumber = new Random().Next(10,100000);
+
+Console.WriteLine($"Изначальное число {rundomNumber}");
+
+if (rundomNumber > 100  & rundomNumber < 1000)
+{
+  int third = rundomNumber % 10;
+  Console.WriteLine($"Третья цифра - {third}");
+}
+
+if (rundomNumber > 1000 & rundomNumber < 10000)
+{
+   int third = rundomNumber % 100 / 10;
+  Console.WriteLine($"Третья цифра - {third}");
+ 
+}
+if (rundomNumber > 10000 & rundomNumber < 100000)
+{
+  int third = rundomNumber / 100 % 10;
+  Console.WriteLine($"Третья цифра - {third}");
+}
+if ( rundomNumber < 100)
+{
+    Console.WriteLine("третьего числа нет");
+}
